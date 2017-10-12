10fc2dede70958602de5828b1d5396863725649a
Aram Simonian <aram.contacts@gmail.com>
2017-10-12 18:01:40 +0100

    Added attack route, attack.erb, link to Attack and P1 attacks P2 test

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,11 @@ feature 'Hit points' do
     sign_in_and_play
     expect(page).to have_content 'Kate: 100 HP'
   end
+
+  scenario 'Player 1 attacks Player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Bob attacks Kate'
+  end
+
 end
