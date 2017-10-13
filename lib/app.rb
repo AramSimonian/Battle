54a03667c4f74e2deca8e894077e0745d516af09
Aram Simonian <aram.contacts@gmail.com>
2017-10-13 16:12:24 +0100

    Refactored to use $game which now requires two players, + new test in game_spec

--- lib/app.rb
+++ lib/app.rb
@@ -12,37 +12,36 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(Player.new(params[:player_1_name]), Player.new(params[:player_2_name]))
 
     redirect to('/play')
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1_name = $game.player1.name
+    @player_2_name = $game.player2.name
 
     erb(:play)
   end
 
   get '/view_hit_points' do
-    @player_1_hit_points = $player_1.hit_points
-    @player_2_hit_points = $player_2.hit_points
+    @player_1_hit_points = $game.player1.hit_points
+    @player_2_hit_points = $game.player2.hit_points
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1_name = $game.player1.name
+    @player_2_name = $game.player2.name
 
     erb :attack
   end
 
   post '/attack' do
-    Game.new.attack($player_2)
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_2_hit_points = $player_2.hit_points
+    $game.attack($game.player2)
+    @player_1_name = $game.player1.name
+    @player_2_name = $game.player2.name
+    @player_2_hit_points = $game.player2.hit_points
 
     erb :attack
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player1, player2) }
   let(:player1) { double(:player1) }
   let(:player2) { double(:player2) }
 
@@ -10,4 +10,8 @@ describe Game do
     game.attack(player2)
   end
 
+  it 'a new game should receive two parameters' do
+    expect(Game).to receive(:new).with(player1, player2)
+    Game.new(player1, player2)
+  end
 end
